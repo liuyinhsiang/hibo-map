ceaf15379ab85c36e5da27d94a1d97ab4b3bb3ca
Martin Liu <liuyinhsiang@martins-mbp.home>
2020-05-01 19:56:28 +1000

    google OAuth added

--- index.js
+++ index.js
@@ -4,10 +4,30 @@ const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
+require('./services/passport');
+
+mongoose
+  .connect(keys.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello'));
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
 
-const PORT = process.env.PORT || 4000;
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
